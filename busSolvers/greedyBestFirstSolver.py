f5bde00d9dbbc7f811c25c3032bb7cf6d67b350f
hanansabo <hanan.sabo@gmail.com>
2017-11-30 16:44:57 +0200

    greedyBestFirstSolver implementation

--- busSolvers/greedyBestFirstSolver.py
+++ busSolvers/greedyBestFirstSolver.py
@@ -1,6 +1,7 @@
 from . import GreedySolver
 import numpy as np
 
+
 class GreedyBestFirstSolver(GreedySolver):
     def __init__(self, roads, astar, scorer):
         super().__init__(roads, astar, scorer)
@@ -8,7 +9,14 @@ class GreedyBestFirstSolver(GreedySolver):
     # Find the next state to develop
     def _getNextState(self, problem, currState):
         successors = list(problem.expand(currState))
-
+        if successors:
+            nextState = successors[0]
+            min = self._scorer.compute(currState, nextState)
+            for s in successors:
+                if self._scorer.compute(currState, s) < min:
+                    min =self._scorer.compute(currState, s)
+                    nextState = s
+        return nextState
         # TODO : Return the next state
         raise NotImplementedError
 
